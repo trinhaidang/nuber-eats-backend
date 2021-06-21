0a2cec8a892f1f58a657223e2c12c8160923b954
trinh <trinh.dang@codestringers.com>
2021-06-21 18:00:55 +0700

    9

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
 import { Restaurant } from "./entities/restaurant.entity";
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantResolver{
-    @Query(returns => Restaurant)
-    myRestaurant(){
-        return true;
+    @Query(returns => [Restaurant])
+    restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[]{
+        console.log(veganOnly);
+        return [];
     }
 }
