39b11d2e4b0ec5d837f7e0e66feb2ab27a8835dc
trinh <trinh.dang@codestringers.com>
2021-06-22 03:04:04 +0700

    15

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5432,
+      "username": "whitecat",
+      "password": "12345",
+      "database": "nuber-eats",
+      "synchronize": true,
+      "logging": true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
