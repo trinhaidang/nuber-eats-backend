8bcec16afc4d4bcbaa18f70ee6cbd5f816308b69
trinh <trinh.dang@codestringers.com>
2021-06-21 20:17:28 +0700

    10

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,11 +1,16 @@
 import { Field, ObjectType } from "@nestjs/graphql";
 
-
 @ObjectType()
-export class Restaurant{
+export class Restaurant {
     @Field(type => String)
     name: string;
-    
-    @Field(type => Boolean, {nullable:true})
-    isGood?:boolean
+
+    @Field(type => Boolean)
+    isVegan: boolean;
+
+    @Field(type => String)
+    address: string;
+
+    @Field(type => String)
+    ownerName: string;
 }
