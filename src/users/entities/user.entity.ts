847375b6fb79722bd5683bbbdd71769be5143a8b
trinh <trinh.dang@codestringers.com>
2021-07-17 13:25:34 +0700

    32 - create user service

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,21 @@
-import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
 import { Column, Entity } from "typeorm";
 
-type UserRole = "client" | "owner" | "delivery"
+enum UserRole {
+    Client,
+    Owner,
+    Delivery,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' })
 
 @InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
     @Column()
+    
     @Field(type => String)
     email: string;
 
@@ -16,8 +23,8 @@ export class User extends CoreEntity {
     @Field(type => String)
     password: string;
 
-    @Column()
-    @Field(type => String)
+    @Column({ type: 'enum', enum: UserRole })
+    @Field(type => UserRole)
     role: UserRole;
 
 }
