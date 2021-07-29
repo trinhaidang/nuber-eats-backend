c376885363344b9f30c4b02efbaf53ecaf753801
trinh <trinh.dang@codestringers.com>
2021-07-29 07:55:57 +0700

    38

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -15,7 +16,9 @@ import { User } from './users/entities/user.entity';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev','prod').required(),
+        NODE_ENV: Joi.string()
+        .valid('dev','prod')
+        .required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -38,6 +41,7 @@ import { User } from './users/entities/user.entity';
       autoSchemaFile: true,
     }),
     UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
