eb8dc543895c8fcbcb59c79c89252e8175d4acd8
trinh <trinh.dang@codestringers.com>
2021-07-31 20:16:33 +0700

    55 - verification entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { CommonModule } from './common/common.module';
 import { JwtModule } from './jwt/jwt.module';
 import { Jwtmiddleware } from './jwt/jwt.middleware';
 import { AuthModule } from './auth/auth.module';
+import { Verification } from './users/entities/verification.entity';
 
 @Module({
   imports: [
@@ -39,7 +40,7 @@ import { AuthModule } from './auth/auth.module';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging:  process.env.NODE_ENV !== 'prod',
-      entities: [User]
+      entities: [User, Verification]
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
