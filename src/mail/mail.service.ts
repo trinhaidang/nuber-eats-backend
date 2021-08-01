4ff1fa53d87029422b4f2935f533299c18defdf1
trinh <trinh.dang@codestringers.com>
2021-08-01 10:08:26 +0700

    63 - create email template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -7,21 +7,23 @@ import * as FormData from 'form-data';
 @Injectable()
 export class MailService {
     constructor(@Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions) {
-        this.sendEmail('testing', 'test', 'trinhaidang@gmail.com');
+        this.sendEmail('testing', 'verify-email', 'trinhaidang@gmail.com');
     }
 
-    private async sendEmail(subject: string, content: string, to: string) {
+    private async sendEmail(subject: string, template: string, to: string) {
         const form = new FormData();
         form.append('from', `Excited User <mailgun@${this.options.domain}>`);
         form.append('to', to);
         form.append('subject', subject);
-        form.append('text', content);
+        form.append('template', template);
+        form.append('v:code', 'asasas');
+        form.append('v:username', 'nico!!!');
         const response = await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
             method: 'POST',
             headers: {
                 "Authorization": `Basic ${Buffer.from(
                     `api:${this.options.apiKey}`
-                ).toString('base64')}`
+                ).toString('base64')}`,
             },
             body: form,
         });
