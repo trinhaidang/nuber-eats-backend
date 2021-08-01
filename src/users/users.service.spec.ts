9992a4e79a6767ff170f3f2d41ae0a311b0423d0
trinh <trinh.dang@codestringers.com>
2021-08-01 21:43:11 +0700

    72 - test login

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -105,9 +105,40 @@ describe("UserService", () => {
             );
             expect(result).toEqual({ ok: true });
         });
+
+        it('should fail on exception', async () => {
+            usersRepository.findOne.mockRejectedValue(new Error('Mock error'));
+            const result = await service.createAccount(createAccountArgs);
+            expect(result).toEqual({
+                ok: false,
+                error: "Couldn't create account"
+            });
+        });
     });
 
-    it.todo('login');
+    describe('login', () => {
+
+        const loginArgs = {
+            email: 'user@mock.com',
+            password: 'mockpass',
+        }
+
+        it('should fail if user does not exist', async () => {
+            usersRepository.findOne.mockResolvedValue(null);
+            const result = await service.login(loginArgs);
+
+            expect(usersRepository.findOne).toHaveBeenCalledTimes(1);
+            expect(usersRepository.findOne).toHaveBeenCalledWith(
+                expect.any(Object),
+                expect.any(Object),
+            );
+            expect(result).toEqual({
+                ok: false,
+                error: 'User not found'
+            });
+        });
+    });
+
     it.todo('findById');
     it.todo('editProfile');
     it.todo('verifyEmail');
