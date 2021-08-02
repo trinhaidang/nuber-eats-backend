dfa792212ddbc0408de5329fc07ddf7ce21e96a0
trinh <trinh.dang@codestringers.com>
2021-08-03 00:42:27 +0700

    84 - setup test e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,7 +43,9 @@ import { MailModule } from './mail/mail.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: process.env.NODE_ENV !== 'prod',
+      logging: 
+        process.env.NODE_ENV !== 'prod' 
+        && process.env.NODE_ENV !== 'test',
       entities: [User, Verification]
     }),
     GraphQLModule.forRoot({
