0e1bd67c14952b7e5f5d804ec14b0ea98bcd4d03
trinh <trinh.dang@codestringers.com>
2021-08-04 13:47:23 +0700

    99 - authen with role

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,18 +1,20 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { GqlExecutionContext } from "@nestjs/graphql";
-import { AllowedRoles } from "./role.decorator";
+import { User } from "src/users/entities/user.entity";
+import { AllowedRoles, Role } from "./role.decorator";
 
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private readonly reflector: Reflector){}
     canActivate(context: ExecutionContext){
-        const role = this.reflector.get<AllowedRoles>('roles', context.getHandler());
-        console.log(role);
+        const roles = this.reflector.get<AllowedRoles>('roles', context.getHandler());
+        if(!roles) return true;
         const gqlContext = GqlExecutionContext.create(context).getContext();
-        const user = gqlContext['user'];
+        const user:User = gqlContext['user'];
         if(!user) return false;
-        return true;
+        if(roles.includes('Any')) return true;
+        return roles.includes(user.role);
     }
 }
