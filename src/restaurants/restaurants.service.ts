c56bf82679d29029d07a63aa35f8217905f4cdf6
trinh <trinh.dang@codestringers.com>
2021-08-04 08:25:43 +0700

    96 - create restaurant

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
-import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
-import { UpdateRestaurantDto } from "./dtos/update-restaurant.dto";
+import { CreateRestaurantInput, CreateRestaurantOutput } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
 
 
 @Injectable()
-export class RestaurantService{
+export class RestaurantService {
 
-    constructor(@InjectRepository(Restaurant) private readonly restaurants: Repository<Restaurant>,){}
-    
-    getAll(): Promise<Restaurant[]>{
-        return this.restaurants.find();
-    }
+    constructor(
+        @InjectRepository(Restaurant)
+        private readonly restaurants: Repository<Restaurant>,
+    ) { }
 
-    createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
-        const newRestaurant = this.restaurants.create(createRestaurantDto);
-        return this.restaurants.save(newRestaurant);
-    }
-
-    updateRestaurant({id, data}: UpdateRestaurantDto) {
-        return this.restaurants.update(id,{ ...data });
+    async createRestaurant(
+        owner: User,
+        createRestaurantInput: CreateRestaurantInput,
+    ): Promise<CreateRestaurantOutput> {
+        try {
+            const newRestaurant = this.restaurants.create(createRestaurantInput);
+            await this.restaurants.save(newRestaurant);
+            return {
+                ok: true,
+            };
+        } catch (error) {
+            console.log(error);
+            return {
+                ok: false,
+                error: "Could not create restaurant"
+            };
+        }
     }
 }
