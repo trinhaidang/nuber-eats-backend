992753e8f781a260ee3dbf396fa4f46f58e63c02
trinh <trinh.dang@codestringers.com>
2021-08-07 20:33:16 +0700

    146 - cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
 import { PaymentsModule } from './payments/payments.module';
 import { Payment } from './payments/entities/payment.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -74,6 +75,7 @@ import { Payment } from './payments/entities/payment.entity';
         };   // return to gqlContext && middleware
       },
     }),
+    ScheduleModule.forRoot(),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
     }),
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -6,8 +6,7 @@ import { User } from "src/users/entities/user.entity";
 import { CreatePaymentInput, CreatePaymentOutput } from "./dtos/create-payment.dto";
 import { Restaurant } from "src/restaurants/entities/restaurant.entity";
 import { GetPaymentsOutput } from "./dtos/get-payments.dto";
-
-
+import { Cron, Interval, Timeout, SchedulerRegistry } from '@nestjs/schedule';
 
 @Injectable()
 export class PaymentService {
@@ -17,19 +16,20 @@ export class PaymentService {
         private readonly payments: Repository<Payment>,
         @InjectRepository(Restaurant)
         private readonly restaurants: Repository<Restaurant>,
-    ){}
+        private readonly schedulerRegistry: SchedulerRegistry,
+    ) { }
 
-    async createPayment(owner: User, {transactionId, restaurantId}: CreatePaymentInput
+    async createPayment(owner: User, { transactionId, restaurantId }: CreatePaymentInput
     ): Promise<CreatePaymentOutput> {
         try {
             const restaurant = await this.restaurants.findOne(restaurantId);
-            if(!restaurant) {
+            if (!restaurant) {
                 return {
                     ok: false,
                     error: 'Restaurant not found.'
                 };
             }
-            if(restaurant.ownerId !== owner.id) {
+            if (restaurant.ownerId !== owner.id) {
                 return {
                     ok: false,
                     error: 'You are not allowed to do this.'
@@ -45,7 +45,7 @@ export class PaymentService {
             return {
                 ok: true
             };
-            
+
         } catch (error) {
             return {
                 ok: false,
@@ -54,9 +54,9 @@ export class PaymentService {
         }
     }
 
-    async getPayments(owner: User): Promise<GetPaymentsOutput>{
+    async getPayments(owner: User): Promise<GetPaymentsOutput> {
         try {
-            const payments = await this.payments.find({user: owner});
+            const payments = await this.payments.find({ user: owner });
             return {
                 ok: true,
                 payments
@@ -67,6 +67,23 @@ export class PaymentService {
                 error: 'Could not get payments'
             }
         }
+    }
+
+    @Cron('30 * * * * *', { name: "myjob" }) // run at the second 30 of every min
+    checkForPayments() {
+        console.log('Checking for payments...(cron)');
+        const job = this.schedulerRegistry.getCronJob('myjob');
+        console.log(job);
+    }
+
+    @Interval(5000)  // run after each 5s
+    checkForPayments1() {
+        console.log('Checking for payments...(interval)');
+    }
+
+    @Timeout(20000)  // run once after start app 20s
+    checkForPayments2() {
+        console.log('Checking for payments...(timeout)');
     }
 
 }
